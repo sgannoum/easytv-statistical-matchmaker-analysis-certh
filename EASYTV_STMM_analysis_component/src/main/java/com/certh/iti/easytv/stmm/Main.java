5d4f65500ae13554a1dc93999ccb4f211780804c
salgan <salgan@iti.gr>
2019-07-08 15:11:50 +0300

    Generate HybridMatchmakerData.js javascript file

--- EASYTV_STMM_analysis_component/src/main/java/com/certh/iti/easytv/stmm/Main.java
+++ EASYTV_STMM_analysis_component/src/main/java/com/certh/iti/easytv/stmm/Main.java
@@ -34,6 +34,8 @@ import com.certh.iti.easytv.user.preference.Preference;
 import com.certh.iti.easytv.user.preference.attributes.AsymmetricBinaryAttribute;
 import com.certh.iti.easytv.user.preference.attributes.Attribute;
 import com.certh.iti.easytv.user.preference.attributes.ColorAttribute;
+import com.certh.iti.easytv.user.preference.attributes.DoubleAttribute;
+import com.certh.iti.easytv.user.preference.attributes.IntegerAttribute;
 import com.certh.iti.easytv.user.preference.attributes.NominalAttribute;
 import com.certh.iti.easytv.user.preference.attributes.NumericAttribute;
 import com.certh.iti.easytv.user.preference.attributes.OrdinalAttribute;
@@ -44,11 +46,11 @@ public class Main {
 	// Arguments
 	private static final String _ArgConfigFile = "-c";
 	private static final String _ArgProfilesDirectory = "-p";
-	private static final String _ArgOutputFile = "-o";
+	private static final String _ArgOutputDirectory = "-o";
 	
 	// Profiles
 	private static File _ConfigFile = null;
-	private static File _OutputFile = null;
+	private static File _OutputDirectory = null;
 	private static File _ProfilesDirectory = null;
 	private static Cluster<UserProfile> _Profiles = new Cluster<UserProfile>();
 
@@ -62,8 +64,8 @@ public class Main {
 				_ProfilesDirectory = new File(value.trim());
 			else if (arg.equals(_ArgConfigFile)) 
 				_ConfigFile = new File(value.trim());
-			else if (arg.equals(_ArgOutputFile)) 
-				_OutputFile = new File(value.trim());
+			else if (arg.equals(_ArgOutputDirectory)) 
+				_OutputDirectory = new File(value.trim());
 		}
 		
 		if(_ProfilesDirectory == null || !_ProfilesDirectory.exists() ) {
@@ -78,10 +80,10 @@ public class Main {
             System.out.println("Could not find profiles directory, reverted to :'" + _ConfigFile.getAbsolutePath() + "'");
 		}
 		
-		if(_OutputFile == null  ) {
+		if(_OutputDirectory == null  ) {
 			String pwd = System.getProperty("user.dir");
-			_OutputFile = new File(_ProfilesDirectory.getAbsolutePath() + File.separator + "StatisticalMatchMakerData.js");
-            System.out.println("Could not find profiles directory, reverted to :'" + _OutputFile.getAbsolutePath() + "'");
+			_OutputDirectory = new File(_ProfilesDirectory.getAbsolutePath() + File.separator + "StatisticalMatchMakerData.js");
+            System.out.println("Could not find profiles directory, reverted to :'" + _OutputDirectory.getAbsolutePath() + "'");
 		}
 		
 		//Check if everything is there
@@ -160,7 +162,7 @@ public class Main {
         }
         
         System.out.println("--------");
-		System.out.println("write JS file to "+_OutputFile.getAbsolutePath());
+		System.out.println("write JS file to "+_OutputDirectory.getAbsolutePath());
 
         //Write JS
         WriteJavaScript(generalized);
@@ -220,7 +222,7 @@ public class Main {
 		for (int i = 0; i < directories.length; i++)
 			ReadProfilesFrom(directories[i]);
 	}
-
+	
 	/**
 	 * Write a javascript file that represent all the given clusters, the file is used by 
 	 * the runtime component to answer matching requests.
@@ -228,11 +230,23 @@ public class Main {
 	 * @throws IOException
 	 */
 	private static void WriteJavaScript(List<UserProfile> clusters) throws IOException {
+		WriteSTMMJavaScript(clusters);
+		WriteHBMMJavaScript(clusters);
+	}
+
+	/**
+	 * Write a javascript file that represent all the given clusters, the file is used by 
+	 * the runtime component to answer matching requests.
+	 * @param clusters
+	 * @throws IOException
+	 */
+	private static void WriteSTMMJavaScript(List<UserProfile> clusters) throws IOException {
 		
-		if(!_OutputFile.exists())
-			_OutputFile.createNewFile();
+		File _stmmOutputFile = new File(_OutputDirectory.getAbsolutePath()+ File.separatorChar + "StatisticalMatchMakerData.js");
+		if(!_stmmOutputFile.exists())
+			_stmmOutputFile.createNewFile();
 				
-		PrintWriter writer = new PrintWriter(_OutputFile);
+		PrintWriter writer = new PrintWriter(_stmmOutputFile);
 		Date now = new Date();
 		DateFormat df = new SimpleDateFormat("dd MMM HH:mm:ss");
 
@@ -263,6 +277,7 @@ public class Main {
         //import classes
         writer.println();
         writer.println("var Numeric = require(\"./DimensionHandlers\").Numeric");
+        writer.println("var IntegerNumeric = require(\"./DimensionHandlers\").IntegerNumeric");
         writer.println("var Nominal = require(\"./DimensionHandlers\").Nominal");
         writer.println("var Ordinal = require(\"./DimensionHandlers\").Ordinal");
         writer.println("var SymmetricBinary = require(\"./DimensionHandlers\").SymmetricBinary");
@@ -281,16 +296,24 @@ public class Main {
 				ColorAttribute colorAttribute = (ColorAttribute) operand;
 				
 				for (NumericAttribute attribte : colorAttribute.getDimensions()) 
-					handlerInstance += "new Numeric("+String.valueOf(attribte.getMaxValue())+", "+ String.valueOf(attribte.getMinValue())+", "+String.valueOf(attribte.getOperandMissingValue())+" ),";
+					handlerInstance += "new IntegerNumeric("+String.valueOf(attribte.getMaxValue())+", "+ String.valueOf(attribte.getMinValue())+", "+String.valueOf(attribte.getOperandMissingValue())+" ),";
 				
 				handlerInstance =  "new Color("+handlerInstance.substring(0, handlerInstance.length() - 1 ) +")";
 				
-			} else if (NumericAttribute.class.isInstance(operand)) {
-				NumericAttribute numeric = (NumericAttribute) operand;
+			}
+        	else if (IntegerAttribute.class.isInstance(operand)) {
+        		IntegerAttribute intNumeric = (IntegerAttribute) operand;
+ 				
+ 				handlerInstance = "new IntegerNumeric("+String.valueOf(intNumeric.getMaxValue())+", "+ String.valueOf(intNumeric.getMinValue())+", "+String.valueOf(intNumeric.getOperandMissingValue())+" )";
+ 				
+ 			}
+        	else if (DoubleAttribute.class.isInstance(operand)) {
+        		DoubleAttribute doubleNumeric = (DoubleAttribute) operand;
 				
-				handlerInstance = "new Numeric("+String.valueOf(numeric.getMaxValue())+", "+ String.valueOf(numeric.getMinValue())+", "+String.valueOf(numeric.getOperandMissingValue())+" )";
+				handlerInstance = "new Numeric("+String.valueOf(doubleNumeric.getMaxValue())+", "+ String.valueOf(doubleNumeric.getMinValue())+", "+String.valueOf(doubleNumeric.getOperandMissingValue())+" )";
 				
-			} else if (OrdinalAttribute.class.isInstance(operand)) {
+			} 
+        	else if (OrdinalAttribute.class.isInstance(operand)) {
 				OrdinalAttribute ordinal = (OrdinalAttribute) operand;
 				
 				String states = "";
@@ -299,7 +322,8 @@ public class Main {
 				
 				handlerInstance = "new Ordinal(["+states.substring(0, states.length() - 1).toLowerCase()+"], "+ String.valueOf(ordinal.getMaxValue())+", "+ String.valueOf(ordinal.getMinValue())+", "+String.valueOf(ordinal.getOperandMissingValue())+" )";
 				
-			} else if (NominalAttribute.class.isInstance(operand)) {
+			} 
+        	else if (NominalAttribute.class.isInstance(operand)) {
 				NominalAttribute nominal = (NominalAttribute) operand;
 				
 				
@@ -309,11 +333,13 @@ public class Main {
 				
 				handlerInstance = "new Nominal(["+states.substring(0, states.length() - 1).toLowerCase()+"], "+String.valueOf(operand.getOperandMissingValue())+")";
 				
-			} else if (SymmetricBinaryAttribute.class.isInstance(operand)) {
+			} 
+        	else if (SymmetricBinaryAttribute.class.isInstance(operand)) {
 				
 				handlerInstance = "new SymmetricBinary("+String.valueOf(operand.getOperandMissingValue())+")";
 
-			} else if (AsymmetricBinaryAttribute.class.isInstance(operand)) {
+			} 
+        	else if (AsymmetricBinaryAttribute.class.isInstance(operand)) {
 				//handlerInstance = new AsymmetricBinary();
 			}
         	
@@ -334,6 +360,118 @@ public class Main {
         }
         
         writer.println("];");        
+        writer.close();
+	}
+	
+	/**
+	 * Write a javascript file that has maps preferences URL to proper dimension handler
+	 * @param clusters
+	 * @throws IOException
+	 */
+	private static void WriteHBMMJavaScript(List<UserProfile> clusters) throws IOException {
+		
+		File _hbmmOutputFile = new File(_OutputDirectory.getAbsolutePath()+ File.separatorChar + "HybridMatchMakerData.js");
+		if(!_hbmmOutputFile.exists())
+			_hbmmOutputFile.createNewFile();
+				
+		PrintWriter writer = new PrintWriter(_hbmmOutputFile);
+		Date now = new Date();
+		DateFormat df = new SimpleDateFormat("dd MMM HH:mm:ss");
+
+        writer.println("/*!");
+        writer.println();
+        writer.println("EasyTV Hybrid Matchmaker ");
+        writer.println();
+        writer.println("Copyright 2017-2020 Center for Research & Technology - HELLAS");
+        writer.println();
+        writer.println("Licensed under the New BSD License. You may not use this file except in");
+        writer.println("compliance with this licence.");
+        writer.println();
+        writer.println("You may obtain a copy of the licence at");
+        writer.println("https://github.com/REMEXLabs/GPII-Statistical-Matchmaker/blob/master/LICENSE.txt");
+        writer.println();
+        writer.println("The research leading to these results has received funding from");
+        writer.println("the European Union's H2020-ICT-2016-2, ICT-19-2017 Media and content convergence");
+        writer.println("under grant agreement no. 761999.");
+        writer.println("*/");
+        writer.println();
+        writer.println("//Generated " +  df.format(now));
+        writer.println("var fluid = fluid || require(\"universal\");");
+        writer.println("var hybrid = fluid.registerNamespace(\"easytv.matchMaker.hybrid\");");
+        
+        //import classes
+        writer.println();
+        writer.println("var Numeric = require(\"./DimensionHandlers\").Numeric");
+        writer.println("var IntegerNumeric = require(\"./DimensionHandlers\").IntegerNumeric");
+        writer.println("var Nominal = require(\"./DimensionHandlers\").Nominal");
+        writer.println("var Ordinal = require(\"./DimensionHandlers\").Ordinal");
+        writer.println("var SymmetricBinary = require(\"./DimensionHandlers\").SymmetricBinary");
+        writer.println("var Color = require(\"./DimensionHandlers\").Color");
+        writer.println();
+
+        
+        writer.println("hybrid.dimensionsHandlers = new Map();");
+
+        //add preference handlers
+        for(Entry<String, Attribute> entry : Preference.preferencesAttributes.entrySet()) {
+        	Attribute operand =  entry.getValue();
+        	String handlerInstance = "";
+        	
+        	if(ColorAttribute.class.isInstance(operand)) {
+				ColorAttribute colorAttribute = (ColorAttribute) operand;
+				
+				for (NumericAttribute attribte : colorAttribute.getDimensions()) 
+					handlerInstance += "new IntegerNumeric("+String.valueOf(attribte.getMaxValue())+", "+ String.valueOf(attribte.getMinValue())+", "+String.valueOf(attribte.getOperandMissingValue())+" ),";
+				
+				handlerInstance =  "new Color("+handlerInstance.substring(0, handlerInstance.length() - 1 ) +")";
+				
+			}
+        	else if (IntegerAttribute.class.isInstance(operand)) {
+        		IntegerAttribute intNumeric = (IntegerAttribute) operand;
+ 				
+ 				handlerInstance = "new IntegerNumeric("+String.valueOf(intNumeric.getMaxValue())+", "+ String.valueOf(intNumeric.getMinValue())+", "+String.valueOf(intNumeric.getOperandMissingValue())+" )";
+ 				
+ 			}
+        	else if (DoubleAttribute.class.isInstance(operand)) {
+        		DoubleAttribute doubleNumeric = (DoubleAttribute) operand;
+				
+				handlerInstance = "new Numeric("+String.valueOf(doubleNumeric.getMaxValue())+", "+ String.valueOf(doubleNumeric.getMinValue())+", "+String.valueOf(doubleNumeric.getOperandMissingValue())+" )";
+				
+			} 
+        	else if (OrdinalAttribute.class.isInstance(operand)) {
+				OrdinalAttribute ordinal = (OrdinalAttribute) operand;
+				
+				String states = "";
+				for(String state : ordinal.getStates())
+					states += "\""+state+"\",";
+				
+				handlerInstance = "new Ordinal(["+states.substring(0, states.length() - 1).toLowerCase()+"], "+ String.valueOf(ordinal.getMaxValue())+", "+ String.valueOf(ordinal.getMinValue())+", "+String.valueOf(ordinal.getOperandMissingValue())+" )";
+				
+			} 
+        	else if (NominalAttribute.class.isInstance(operand)) {
+				NominalAttribute nominal = (NominalAttribute) operand;
+				
+				
+				String states = "";
+				for(String state : nominal.getStates())
+					states += "\""+state+"\",";
+				
+				handlerInstance = "new Nominal(["+states.substring(0, states.length() - 1).toLowerCase()+"], "+String.valueOf(operand.getOperandMissingValue())+")";
+				
+			} 
+        	else if (SymmetricBinaryAttribute.class.isInstance(operand)) {
+				
+				handlerInstance = "new SymmetricBinary("+String.valueOf(operand.getOperandMissingValue())+")";
+
+			} 
+        	else if (AsymmetricBinaryAttribute.class.isInstance(operand)) {
+				//handlerInstance = new AsymmetricBinary();
+			}
+        	
+        	 writer.println("hybrid.dimensionsHandlers.set(\"" +entry.getKey() + "\", "+ handlerInstance +");");
+        }
+        
+        writer.println();
         writer.close();
 	}
 
